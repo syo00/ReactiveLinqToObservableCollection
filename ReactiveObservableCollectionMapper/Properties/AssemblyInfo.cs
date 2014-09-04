ac14d8814fb84625be2c50960c666a2d86dc0c7a
syo00 <syo00@outlook.com>
2014-09-05 01:45:16 +0900

    fix AssemblyFileVersion

--- ReactiveObservableCollectionMapper/Properties/AssemblyInfo.cs
+++ ReactiveObservableCollectionMapper/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyInformationalVersion("0.0 beta")]
